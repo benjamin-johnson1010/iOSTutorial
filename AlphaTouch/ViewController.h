c847ab65cae31fc726084f5ded355b9e91348c0f
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2016-12-02 20:13:03 -0600

    Initial Commit

---
+++ AlphaTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlphaTouch
+//
+//  Created by BENJAMIN JOHNSON on 12/2/16.
+//  Copyright © 2016 BENJAMIN JOHNSON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
